086df9e1d1ce29a6aff9f5aa872f4f75ef2441f1
Stefan Hendriks <stefan@fundynamic.com>
2019-09-26 06:48:34 +0200

    only take 'contact' into account for path finding. Need to revisit this heuristic anyways

--- NodeMachine.cpp
+++ NodeMachine.cpp
@@ -2441,10 +2441,10 @@ void cNodeMachine::openNeighbourNodes(int startNodeIndex, int nodeToOpenNeighbou
 
         if (botTeam > -1) {
             double dangerCost = InfoNodes[neighbourNode].fDanger[botTeam] * cost;
-            double contactCost = InfoNodes[neighbourNode].fContact[botTeam] * cost;
+//            double contactCost = InfoNodes[neighbourNode].fContact[botTeam] * cost;
 
             cost += dangerCost;
-            cost += contactCost;
+//            cost += contactCost;
         }
 
         // TODO: Add costs for how easy it is to navigate (ie some kind of arrival speed score? if given?)
