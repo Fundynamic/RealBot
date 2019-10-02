8613aa67aac4eb0eb4f5472a96e94a18590c7a0c
Stefan Hendriks <stefan@fundynamic.com>
2019-10-02 07:03:43 +0200

    Started testing on as_oilrig again

--- makeAndCopyAndRun.sh
+++ makeAndCopyAndRun.sh
@@ -17,7 +17,9 @@ touch "/Users/shendriks/Library/Application Support/Steam/SteamApps/common/Half-
 # run
 #"/Users/shendriks/Library/Application Support/Steam/SteamApps/common/Half-Life/hl.sh" -game cstrike --listen +map cs_italy +maxplayers 32
 #"/Users/shendriks/Library/Application Support/Steam/SteamApps/common/Half-Life/hl.sh" -game cstrike --listen +map de_prodigy +maxplayers 32
-"/Users/shendriks/Library/Application Support/Steam/SteamApps/common/Half-Life/hl.sh" -game cstrike --listen +map de_dust +maxplayers 32
-#"/Users/shendriks/Library/Application Support/Steam/SteamApps/common/Half-Life/hl.sh" -game cstrike --listen +map as_oilrig +maxplayers 32
+#"/Users/shendriks/Library/Application Support/Steam/SteamApps/common/Half-Life/hl.sh" -game cstrike --listen +map de_dust +maxplayers 32
+
+"/Users/shendriks/Library/Application Support/Steam/SteamApps/common/Half-Life/hl.sh" -game cstrike --listen +map as_oilrig +maxplayers 32
+
 #"/Users/shendriks/Library/Application Support/Steam/SteamApps/common/Half-Life/hl.sh" -game cstrike --listen +map fy_iceworld +maxplayers 32
 #"/Users/shendriks/Library/Application Support/Steam/SteamApps/common/Half-Life/hl.sh" -game cstrike --listen +map cs_assault +maxplayers 32
